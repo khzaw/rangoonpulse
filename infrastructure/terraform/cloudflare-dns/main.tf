a62fc2fbc53c6f1648a3884dcbb4e0de324cef35
Kaung Htet <kayhzaw@gmail.com>
2025-04-05 19:53:31 +0800

    feat: Add Terraform config for Cloudflare DNS via tofu-controller

---
+++ infrastructure/terraform/cloudflare-dns/main.tf
@@ -0,0 +1,49 @@
+data "cloudflare_zone" "primary" {
+  name    = var.domain_name
+}
+
+resource "cloudflare_record" "longhorn" {
+  zone_id = data.cloudflare_zone.primary.id
+  name    = "longhorn"
+  value   = var.metallb_ip
+  type    = "A"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "uptime_kuma" {
+  zone_id = data.cloudflare_zone.primary.id
+  name    = "uptime-kuma"
+  value   = var.metallb_ip
+  type    = "A"
+  ttl     = 1
+  proxied = false
+}
+
+
+resource "cloudflare_record" "calibre_manage" {
+  zone_id = data.cloudflare_zone.primary.id
+  name    = "calibre-manage"
+  value   = var.metallb_ip
+  type    = "A"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "calibre_web" {
+  zone_id = data.cloudflare_zone.primary.id
+  name    = "calibre"
+  value   = var.metallb_ip
+  type    = "a"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "actual_budget" {
+  zone_id = data.cloudflare_zone.primary.id
+  name    = "budget"
+  value   = var.metallb_ip
+  type    = "A"
+  ttl     = 1
+  proxied = false
+}
---
+++ infrastructure/terraform/cloudflare-dns/variables.tf
@@ -0,0 +1,24 @@
+variable "cloudflare_api_token" {
+  type        = string
+  description = "Cloudflare API Token"
+  sensitive   = true
+}
+
+
+variable "cloudflare_zone_id" {
+ type        = string
+ description = "Cloudflare Zone ID for the domain (provided by Flux TF object)"
+}
+
+
+variable "metallb-ip" {
+  type        = string
+  description = "The External IP assigned by MetalLB to ingress-nginx"
+  default     = "10.254.250.0"
+}
+
+variable "domain_name" {
+  type        = "string"
+  description =" domain name"
+  default     = "khzaw.dev"
+}
