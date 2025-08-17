8aea58837961ee10131a5221c00b8084d977d246
Kaung Htet <kayhzaw@gmail.com>
2025-08-17 23:16:46 +0800

    Get rid of longhorn

--- infrastructure/terraform/cloudflare-dns/main.tf
+++ infrastructure/terraform/cloudflare-dns/main.tf
@@ -2,15 +2,6 @@ data "cloudflare_zone" "primary" {
   name    = var.domain_name
 }
 
-resource "cloudflare_record" "longhorn" {
-  zone_id = data.cloudflare_zone.primary.id
-  name    = "longhorn"
-  content = var.metallb_ip
-  type    = "A"
-  ttl     = 1
-  proxied = false
-}
-
 resource "cloudflare_record" "uptime_kuma" {
   zone_id = data.cloudflare_zone.primary.id
   name    = "uptime-kuma"
@@ -48,11 +39,11 @@ resource "cloudflare_record" "actual_budget" {
   proxied = false
 }
 
-resource "cloudflare_record" "ghost" {
-  zone_id = data.cloudflare_zone.primary.id
-  name = "blog"
-  value = var.metallb_ip
-  type = "A"
-  ttl = 1
-  proxied = false
-}
+# resource "cloudflare_record" "ghost" {
+#   zone_id = data.cloudflare_zone.primary.id
+#   name = "blog"
+#   value = var.metallb_ip
+#   type = "A"
+#   ttl = 1
+#   proxied = false
+# }
